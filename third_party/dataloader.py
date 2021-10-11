b98708aa02cd7d56ac0ac7582524992f82ba1bc7
Alexey <alexey.s.russ@mail.ru>
2021-10-11 23:04:08 +0300

    Add weight sampler

--- third_party/dataloader.py
+++ third_party/dataloader.py
@@ -8,6 +8,41 @@ from tqdm import tqdm
 import pandas as pd
 
 
+def make_weights_for_balanced_classes(dataset_table, nclasses):
+    '''
+        Make a vector of weights for each image in the dataset, based
+        on class frequency. The returned vector of weights can be used
+        to create a WeightedRandomSampler for a DataLoader to have
+        class balancing when sampling for a training batch.
+            images - torchvisionDataset.imgs
+            nclasses - len(torchvisionDataset.classes)
+        https://discuss.pytorch.org/t/balanced-sampling-between-classes-with-torchvision-dataloader/2703/3
+    '''
+
+    count = [0] * nclasses
+
+    for item_idx in range(len(dataset_table)):
+        count[dataset_table.loc[item_idx, 'brick_class']] += 1  # item is (img-data, label-id)
+
+    weight_per_class = [0.] * nclasses
+
+    N = float(sum(count))  # total number of images
+
+    for i in range(nclasses):
+        d = float(count[i])
+        if count[i] == 0:
+            d = 1
+        weight_per_class[i] = N / d
+
+    weight = [0] * len(dataset_table)
+
+    for item_idx in range(len(dataset_table)):
+        val = dataset_table.loc[item_idx, 'brick_class']
+        weight[item_idx] = weight_per_class[val]
+
+    return weight
+
+
 class BricksDataloader(torch.utils.data.Dataset):
     def __init__(self,
                  root_path: str, shape: tuple,
@@ -118,6 +153,14 @@ class BricksDataloader(torch.utils.data.Dataset):
         if self.augmentations is not None:
             return torch.clamp(self.augmentations(img), 0, 1)
         return self.preprocessing(img)
+
+    def get_classes_sampler(self):
+        weights = make_weights_for_balanced_classes(self.images_dataframe,
+                                                    self.num_classes)
+        weights = torch.DoubleTensor(weights)
+        sampler = torch.utils.data.sampler.WeightedRandomSampler(
+            weights, len(weights))
+        return sampler
 
     def __getitem__(self, idx):
         img_path = self.images_dataframe.loc[idx, 'img_path']
