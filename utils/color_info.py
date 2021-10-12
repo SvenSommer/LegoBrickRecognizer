f6f136af9d41174baccbe201c805e002f79e52f9
robert <rob.hoffmann@posteo.de>
2021-10-12 20:06:17 +0200

    added HEX Code for colors

--- utils/color_info.py
+++ utils/color_info.py
@@ -7,21 +7,22 @@ class ColorInfo:
         self.cur = cursor
 
     def get_colors(self):
-        self.cur.execute("""SELECT color_id, color_name, color_type  FROM LegoSorterDB.Colors where parts_count > 99""")
+        self.cur.execute("""SELECT color_id, color_name, color_type, color_code  FROM LegoSorterDB.Colors where parts_count > 99""")
         sqlresult = self.cur.fetchall()
 
         return_colors = []
         for row in sqlresult:
-            return_colors.append(Color(row[0], row[1], row[2]))
+            return_colors.append(Color(row[0], row[1], row[2], row[3]))
 
         return return_colors
 
 
 class Color:
-    def __init__(self, color_id, color_name, color_type):
+    def __init__(self, color_id, color_name, color_type, color_code):
         self.color_id = color_id
         self.color_name = color_name
         self.color_type = color_type
+        self.color_code = color_code
 
     def __str__(self):
-        return "{} {}({})".format(self.color_type, self.color_name, self.color_id)
+        return "{} {}({}) [{}]".format(self.color_type, self.color_name, self.color_id, self.color_code)
