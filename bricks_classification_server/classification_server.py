91817a7afed296946c8c53c47f054df9a89d438a
Alexey <alexey.s.russ@mail.ru>
2021-11-10 14:27:55 +0300

    Add square padding

--- bricks_classification_server/classification_server.py
+++ bricks_classification_server/classification_server.py
@@ -29,6 +29,88 @@ def args_parse() -> Namespace:
     return parser.parse_args()
 
 
+def create_square_crop_by_detection(
+        frame: np.ndarray,
+        box: list,
+        return_shifts: bool = False,
+        zero_pad: bool = True):
+    """
+    Rebuild detection box to square shape
+    Args:
+        frame: rgb image in np.uint8 format
+        box: list with follow structure: [x1, y1, x2, y2]
+        return_shifts: if set True then function return tuple of image crop
+           and (x, y) tuple of shift coordinates
+        zero_pad: pad result image by zeros values
+
+    Returns:
+        Image crop by box with square shape or tuple of crop and shifted coords
+    """
+    w = box[2] - box[0]
+    h = box[3] - box[1]
+    cx = box[0] + w // 2
+    cy = box[1] + h // 2
+    radius = max(w, h) // 2
+    exist_box = []
+    pads = []
+
+    # y top
+    if cy - radius >= 0:
+        exist_box.append(cy - radius)
+        pads.append(0)
+    else:
+        exist_box.append(0)
+        pads.append(-(cy - radius))
+
+    # y bottom
+    if cy + radius >= frame.shape[0]:
+        exist_box.append(frame.shape[0] - 1)
+        pads.append(cy + radius - frame.shape[0] + 1)
+    else:
+        exist_box.append(cy + radius)
+        pads.append(0)
+    # x left
+    if cx - radius >= 0:
+        exist_box.append(cx - radius)
+        pads.append(0)
+
+    else:
+        exist_box.append(0)
+        pads.append(-(cx - radius))
+
+    # x right
+    if cx + radius >= frame.shape[1]:
+        exist_box.append(frame.shape[1] - 1)
+        pads.append(cx + radius - frame.shape[1] + 1)
+    else:
+        exist_box.append(cx + radius)
+        pads.append(0)
+
+    exist_crop = frame[
+                 exist_box[0]:exist_box[1],
+                 exist_box[2]:exist_box[3]
+                 ].copy()
+
+    croped = np.pad(
+        exist_crop,
+        (
+            (pads[0], pads[1]),
+            (pads[2], pads[3]),
+            (0, 0)
+        ),
+        'reflect' if not zero_pad else 'constant',
+        constant_values=0
+    )
+
+    if not return_shifts:
+        return croped
+
+    shift_x = exist_box[2] - pads[2]
+    shift_y = exist_box[0] - pads[0]
+
+    return croped, (shift_x, shift_y)
+
+
 class FunctionServingWrapper(object):
     """
     Class of wrapper for restriction count of simultaneous function calls
@@ -91,7 +173,13 @@ class BrickClassification(object):
         )
 
     def _preprocess(self, _input_img: Image) -> torch.Tensor:
-        inp_tensor = self.transform(_input_img).unsqueeze(0).to(self.device)
+        _image = np.array(_input_img.convert('RGB'))
+        _image = create_square_crop_by_detection(
+            _image,
+            [0, 0, _image.shape[1], _image.shape[0]]
+        )
+        _image = Image.fromarray(_image)
+        inp_tensor = self.transform(_image).unsqueeze(0).to(self.device)
         return inp_tensor
 
     def __call__(self, _img: Image) -> tuple:
--- third_party/dataloader.py
+++ third_party/dataloader.py
@@ -43,6 +43,88 @@ def make_weights_for_balanced_classes(dataset_table, nclasses):
     return weight
 
 
+def create_square_crop_by_detection(
+        frame: np.ndarray,
+        box: list,
+        return_shifts: bool = False,
+        zero_pad: bool = True):
+    """
+    Rebuild detection box to square shape
+    Args:
+        frame: rgb image in np.uint8 format
+        box: list with follow structure: [x1, y1, x2, y2]
+        return_shifts: if set True then function return tuple of image crop
+           and (x, y) tuple of shift coordinates
+        zero_pad: pad result image by zeros values
+
+    Returns:
+        Image crop by box with square shape or tuple of crop and shifted coords
+    """
+    w = box[2] - box[0]
+    h = box[3] - box[1]
+    cx = box[0] + w // 2
+    cy = box[1] + h // 2
+    radius = max(w, h) // 2
+    exist_box = []
+    pads = []
+
+    # y top
+    if cy - radius >= 0:
+        exist_box.append(cy - radius)
+        pads.append(0)
+    else:
+        exist_box.append(0)
+        pads.append(-(cy - radius))
+
+    # y bottom
+    if cy + radius >= frame.shape[0]:
+        exist_box.append(frame.shape[0] - 1)
+        pads.append(cy + radius - frame.shape[0] + 1)
+    else:
+        exist_box.append(cy + radius)
+        pads.append(0)
+    # x left
+    if cx - radius >= 0:
+        exist_box.append(cx - radius)
+        pads.append(0)
+
+    else:
+        exist_box.append(0)
+        pads.append(-(cx - radius))
+
+    # x right
+    if cx + radius >= frame.shape[1]:
+        exist_box.append(frame.shape[1] - 1)
+        pads.append(cx + radius - frame.shape[1] + 1)
+    else:
+        exist_box.append(cx + radius)
+        pads.append(0)
+
+    exist_crop = frame[
+                 exist_box[0]:exist_box[1],
+                 exist_box[2]:exist_box[3]
+                 ].copy()
+
+    croped = np.pad(
+        exist_crop,
+        (
+            (pads[0], pads[1]),
+            (pads[2], pads[3]),
+            (0, 0)
+        ),
+        'reflect' if not zero_pad else 'constant',
+        constant_values=0
+    )
+
+    if not return_shifts:
+        return croped
+
+    shift_x = exist_box[2] - pads[2]
+    shift_y = exist_box[0] - pads[0]
+
+    return croped, (shift_x, shift_y)
+
+
 class BricksDataloader(torch.utils.data.Dataset):
     def __init__(self,
                  root_path: str, shape: tuple,
@@ -163,6 +245,11 @@ class BricksDataloader(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         img_path = self.images_dataframe.loc[idx, 'img_path']
         brick_class = self.images_dataframe.loc[idx, 'brick_class']
-        image = Image.open(img_path)
+        image = np.array(Image.open(img_path).convert('RGB'))
+        image = create_square_crop_by_detection(
+            image,
+            [0, 0, image.shape[1], image.shape[0]]
+        )
+        image = Image.fromarray(image)
 
         return self.apply_augmentations(image), brick_class
